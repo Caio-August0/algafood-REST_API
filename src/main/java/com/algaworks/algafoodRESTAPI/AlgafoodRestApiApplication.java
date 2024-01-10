969ec6a8c68c0885abbab5abfed4cf5b244ee284
Caio Augusto <caioaugustobr0106@gmail.com>
2024-01-10 09:47:05 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/algaworks/algafoodRESTAPI/AlgafoodRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodRESTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/algafoodRESTAPI/AlgafoodRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodRESTAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
